2876b05c65f48a9738fddaf1ef0cc388ee7a06cc
Mehdi Rahim <mehdi.rahim@cea.fr>
2015-05-28 17:08:56 +0200

    conn classifier

--- test_connectivity_classifier.py
+++ test_connectivity_classifier.py
@@ -5,13 +5,18 @@ Created on Thu May 28 10:52:46 2015
 @author: mehdi.rahim@cea.fr
 """
 
-from fetch_data import fetch_adni_baseline_rs_fmri, fetch_adni_masks
+import numpy as np
+from fetch_data import fetch_adni_baseline_rs_fmri, fetch_adni_masks, \
+                        fetch_adni_longitudinal_rs_fmri
 from base_connectivity_classifier import ConnectivityClassifier
 
-dataset = fetch_adni_baseline_rs_fmri()
+#dataset = fetch_adni_baseline_rs_fmri()
+dataset = fetch_adni_longitudinal_rs_fmri()
 mask = fetch_adni_masks()['mask_petmr']
 
 connclassif = ConnectivityClassifier(dataset.func, mask, atlas='msdl',
                                      dx_group=dataset.dx_group, n_jobs=20)
 connclassif.compute_connectivity('corr')
-connclassif.classify(groups=['AD', 'Normal'])
+score = connclassif.classify(groups=['AD', 'Normal'])
+
+np.save('scores_longitudinal', score.scores_)
