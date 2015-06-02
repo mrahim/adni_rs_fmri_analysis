fb25db3a59eea6edf1a9ac4fb033a22021b564e0
mrahim <rahim.mehdi@gmail.com>
2015-06-02 19:54:48 +0200

    subject shuffle split

--- base_connectivity_classifier.py
+++ base_connectivity_classifier.py
@@ -9,7 +9,7 @@ from joblib import Parallel, delayed
 from sklearn.linear_model import LogisticRegression, RidgeClassifierCV
 from sklearn.svm import LinearSVC
 from sklearn.datasets.base import Bunch
-from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.cross_validation import StratifiedShuffleSplit, ShuffleSplit
 from fetch_data import set_group_indices, set_cache_base_dir
 from sklearn.base import BaseEstimator, TransformerMixin
 from base_connectivity import Connectivity
@@ -19,6 +19,40 @@ CACHE_DIR = set_cache_base_dir()
 ###############################################################################
 # Classification
 ###############################################################################
+def SubjectShuffleSplit(dataset, groups, n_iter=100,
+                        test_size=.3, random_state=42):
+    """ Specific ShuffleSplit (train on all subject images,
+    but test only on one image of the remaining subjects)"""
+
+    idx = set_group_indices(dataset.dx_group)
+    groups_idx = np.hstack((idx[groups[0]],
+                            idx[groups[1]]))
+
+    subjects = np.asarray(dataset.subjects)
+    subjects = subjects[groups_idx]
+    subjects_unique = np.unique(subjects)
+
+    n = len(subjects_unique)
+    ss = ShuffleSplit(n, n_iter=n_iter,
+                      test_size=test_size, random_state=random_state)
+
+    subj_ss = []
+    for train, test in ss:
+        train_set = np.array([], dtype=int)
+        for subj in subjects_unique[train]:
+            subj_ind = np.where(subjects == subj)
+            train_set = np.concatenate((train_set, subj_ind[0]))
+        test_set = []
+        for subj in subjects_unique[test]:
+            subj_ind = np.where(subjects == subj)
+            test_set.append(subj_ind[0][0])
+        test_set = np.asarray(test_set)
+        subj_ss.append([train_set, test_set])
+
+    return subj_ss
+
+
+
 def train_and_test(classifier, X, y, train, test):
     """ Returns accuracy and coeffs for a train and test
     """
@@ -79,7 +113,7 @@ class ConnectivityClassifier(BaseEstimator, TransformerMixin):
         self.connectivity = conn.fit(self.imgs)
         
     
-    def classify(self, groups=['AD', 'MCI'], classifier_name='logreg_l2'):
+    def classify(self, dataset=None, groups=['AD', 'MCI'], classifier_name='logreg_l2'):
         """ Returns accuracy scores
         """
         if hasattr(self, 'connectivity'):        
@@ -88,9 +122,15 @@ class ConnectivityClassifier(BaseEstimator, TransformerMixin):
             X = self.connectivity[groups_idx, :]
             y = np.asarray([+1] * len(self.idx[groups[0]]) +
                            [-1] * len(self.idx[groups[1]]))
-            sss = StratifiedShuffleSplit(y, n_iter=self.n_iter,
-                                         test_size=.25,
-                                         random_state=self.random_state)
+                           
+            if dataset is None:
+                sss = StratifiedShuffleSplit(y, n_iter=self.n_iter,
+                                             test_size=.25,
+                                             random_state=self.random_state)
+            else:
+                sss = SubjectShuffleSplit(dataset, groups, n_iter=self.n_iter,
+                                          test_size=.3,
+                                          random_state=self.random_state)
             results = classify_connectivity(X, y, sss, classifier_name,
                                             n_jobs=self.n_jobs)
             self.scores_ = []
